4a12d1f3e473f69b3a409077a6b86a9ac72e0b94
Dilip Kumar BV <65397257+dilipspartan@users.noreply.github.com>
2023-11-02 20:48:08 +0530

    Add files via upload

---
+++ 2ndoct2023/Lab1.py
@@ -0,0 +1,5 @@
+print("Hello World!!")
+print(4 + 5)
+print(4 * 5)
+
+print(400 * 30)
---
+++ 2ndoct2023/Lab2.py
@@ -0,0 +1,15 @@
+#True
+#true --- this is a comment
+# python is case-sensitive language, both above are different
+# to indent in python right click-->select reformat code
+
+#comments are always ignore comments
+#and will not be run and no outuput can be displayed for comments
+
+''' this is also a way to comment with
+ multiple lines'''
+print(123 + 224)
+print(143 + 225)
+print(99 + 226) #ctrl + D is for duplicate lines
+
+# exit code 0 is successful execution of program without error and o/p
